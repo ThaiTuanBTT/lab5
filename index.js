b21f6a92fbd70318a83436dc1c09f39e720f91f2
ThaiTuanBTT <tuanbtgch200717@fpt.edu.vn>
2022-09-24 17:24:06 +0700

    ok

--- index.js
+++ index.js
@@ -16,15 +16,17 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-    var name = "Cloud Computing";
-    var code = "1644";
+    var name = "Cloud Computing"; //string
+    var code = "1644"; //int
     //pass dữ liệu từ back-end sang front-end
     //cú pháp: name(data) : value(data)
+    //gửi dữ liệu là value và nhận với tên là name
     res.render("index", { name: name, ma: code, ky: "Fall 2022" });
 });
 
 app.get("/mobile", (req, res) => {
-    res.render("mobile");
+    var list = ["iphone", "Samsung", "Oppo", "Xiaomi"];
+    res.render("mobile", { mobile: list });
 });
 
 app.get("/laptop", (req, res) => {

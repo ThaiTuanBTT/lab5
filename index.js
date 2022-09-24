ef5430ce1c4305a62628a671800cc6bb914533b6
ThaiTuanBTT <tuanbtgch200717@fpt.edu.vn>
2022-09-24 17:01:38 +0700

    ok

---
+++ index.js
@@ -0,0 +1,31 @@
+//Khao báo module express framework
+const express = require("express");
+const app = express();
+//khai báo module path => để set đường dẫn thư mục chứa view
+const path = require("path");
+
+//khai báo server  port
+const port = process.eventNames.PORT || 3000;
+
+//set thư mục chứa view
+app.set("views", path.join(__dirname, "views"));
+
+//setview engine(template engine)
+//=>giúp gõ codecủa back-endngay trong front-end dễ hơn
+//hbs:handle bar => nếu dùng view engine khác thì thay đổi giá trị này.
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/mobile", (req, res) => {
+    res.render("mobile");
+});
+
+app.get("/laptop", (req, res) => {
+    res.render("laptop");
+});
+
+//listen port để chạy server
+app.listen(port);
